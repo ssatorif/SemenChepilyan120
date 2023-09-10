2bbbd8a96516ff6f85973f3c823ebe49694db202
ssatorif <99587720+ssatorif@users.noreply.github.com>
2023-09-10 19:55:58 +0700

    9.09.23

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+//Чепилян Семен Вариант 20
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Программа для автоматического обновления операционной системы запущена");
+
+        if (IsWindows())
+        {
+            Console.WriteLine("Обнаружена ОС Windows");
+            RunWindowsUpdate();
+        }
+        Console.WriteLine("Программа завершена");
+    }
+
+    static bool IsWindows()
+    {
+        return Environment.OSVersion.Platform == PlatformID.Win32NT;
+    }
+
+    static void RunWindowsUpdate()
+    {
+        try
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/C wuauclt /detectnow"; // поиск и установка обновлений
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine("Результат обновления Windows:\n" + output);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при обновлении Windows: {ex.Message}");
+        }
+    }
+}
