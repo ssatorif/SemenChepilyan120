04122b8ce02cda52f43c3e23609d47e4f12f17b1
ssatorif <99587720+ssatorif@users.noreply.github.com>
2023-09-11 11:12:59 +0700

    11.09.23
    Для отображения ввести следующую команду в PowerShell: net start perfsvc

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Системный монитор:");
+        Console.WriteLine("Нажмите Ctrl+C для завершения.");
+
+        while (!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.C)
+        {
+            // Мониторим загрузку CPU
+            float cpuUsage = GetCpuUsage();
+            Console.WriteLine($"CPU: {cpuUsage}%");
+
+            // Мониторим доступную оперативную память (RAM)
+            float ramAvailable = GetAvailableRam();
+            Console.WriteLine($"RAM: {ramAvailable} MB");
+
+            // Пауза между обновлениями
+            Thread.Sleep(1000);
+        }
+    }
+
+    static float GetCpuUsage()
+    {
+        using (PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total"))
+        {
+            cpuCounter.NextValue(); // Инициализация
+            Thread.Sleep(1000); // Задержка
+            return cpuCounter.NextValue();
+        }
+    }
+
+    static float GetAvailableRam()
+    {
+        using (PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes"))
+        {
+            return ramCounter.NextValue();
+        }
+    }
+}
